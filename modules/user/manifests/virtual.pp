2f6d45c6d74d7546d34d0fc0e3bf89eeb02ae3c7
Aron Szekely <aszekely@freelancersunion.org>
2014-05-09 17:32:43 -0400

    empty wheel does not work

--- modules/user/manifests/virtual.pp
+++ modules/user/manifests/virtual.pp
@@ -8,10 +8,11 @@ class user::virtual {
 		}
 	}
 
-	define ssh_user($key,$dotfile='') {
+	define ssh_user($key,$dotfile='',$groups='') {
 		user { $name:
 			ensure => present,
 			managehome => true,
+			groups => $groups,
 		}
 
 		file { "/home/${name}/.ssh":
@@ -38,5 +39,6 @@ class user::virtual {
 	@ssh_user { 'aron':
 		key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC8sz27v+9OGveBidk3ghXKVgtGMU9PficS+NJz+5mCRh0MQKpHIlUHMwfJWQkBpQmvFKqeXGVojAeMO8Nf6BlWqP3jt50Gfi2daVOOh6tP+yPg9yILDqDKaOteAWm0uX7byOYQ0y9LNSEjhncboez8g05jTZsBegpAqIBrNlKOqzdCRKooVsJ0owuqeN0/O29XOG4MKEGJ0ojzdffOJcHiJG4+/T2YN9wYvlxhilBUcE+CFrlUzRHhZsRPTniBWdtSGT3WNSzSHkQsOGly5PXzOiL/L6HcRlYQpEopRFVmQe179mpsu8l6QLG8G9nS+lVtOPGNJ0+3V90JGdELCGi9',
 		dotfile => ['forward'],
+		#groups => ['wheel'],
 	}
 }
