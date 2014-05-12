8c795268446abb218b228b5a08b05664e4417ef2
Aron Szekely <aron@indispeq.com>
2014-05-12 00:12:30 -0400

    calling it a night

--- modules/freeswitch-fail2ban/manifests/install.pp
+++ modules/freeswitch-fail2ban/manifests/install.pp
@@ -1,7 +1,15 @@
 class freeswitch-fail2ban::install {
 
+	#do this if centos
+
 	package { 'fail2ban':
 		require => Package ['epel-release'],
 		ensure => installed,
 	}
+
+	#do this for ubuntu
+
+	# ----------
+	#get config stuff from here
+	#https://confluence.freeswitch.org/display/FREESWITCH/Fail2Ban
 }
