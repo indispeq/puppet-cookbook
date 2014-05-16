106c1f34b7c4c90766a5484e9b431d888d72a908
Aron Szekely <aszekely@freelancersunion.org>
2014-05-16 12:57:01 -0400

    sysstat removed some stuff from diagnostics

--- modules/indispeq/manifests/diagnostics.pp
+++ modules/indispeq/manifests/diagnostics.pp
@@ -6,11 +6,11 @@ class indispeq::diagnostics {
 
 	#revisit this, am getting a Notice:
 	#/Stage[main]/Indispeq::Diagnostics/Service[sysstat]/ensure: ensure changed 'stopped' to 'running', like it's not chkconfig-d
-	service { 'sysstat':
-		require => Package['sysstat'],
-		enable => true,
-		ensure => running,
-	}
+#	service { 'sysstat':
+#		require => Package['sysstat'],
+#		enable => true,
+#		ensure => running,
+#	}
 
 	#may add parameters to change /etc/sysconfig/sysstat, or whatever ubuntu equivalent, then notify service
 
