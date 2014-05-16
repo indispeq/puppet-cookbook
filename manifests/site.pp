db7e6aee802f2c5858854dd46f0cbe154556f6dd
Aron Szekely <aszekely@freelancersunion.org>
2014-05-16 12:11:09 -0400

    added papply for fs1 in centos friendly location

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,21 @@
-node 'centos1.indispeq.com', 'fs1.indispeq.local' { 
+node 'centos1.indispeq.com' { 
 	include indispeq::diagnostics
 	include indispeq::security
 	include user::virtual
 	include user::sysadmins
 	include freeswitch::install
 	include epel
+
 	#fail2ban keeps adding extra rules to iptables. not cool. disabled for now till I find the problem
 	#include fail2ban::install
+}
+
+node 'fs1.indispeq.local' {
+	include indispeq::puppet 
+	include indispeq::diagnostics
+	include indispeq::security
+	include user::virtual
+	include user::sysadmins
+	include freeswitch::install
+	include epel
 }
